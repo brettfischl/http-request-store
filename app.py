80ec9a5cb2eb8da50e46f169849afb9a66dec48e
Brett Fischl <bfischl@developertown.com>
2018-10-23 13:58:31 -0400

    add all files

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+
+import db
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+    
+    
+@app.route('/testdb')
+def test():
+    return db.test()
+    
+@app.route('/request', methods=['post'])
+def receivePost():
+    # data = request.data
+    return db.receivePost(request)
