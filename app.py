624a4addfda51f2454fedf3979e3c7850e5204db
brettfischl <brettfischl1@gmail.com>
2018-10-30 22:11:55 -0400

    allow cors

--- app.py
+++ app.py
@@ -4,15 +4,22 @@ app = Flask(__name__)
 
 import db
 
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+  return response
+
 @app.route("/")
 def hello():
     return "Hello World!"
-    
-    
+
+
 @app.route('/testdb')
 def test():
     return db.test()
-    
+
 @app.route('/request', methods=['post'])
 def receivePost():
     # data = request.data
--- credentials.py
+++ credentials.py
@@ -2,7 +2,7 @@ import os
 db_url = os.environ.get('DATABASE_URL', None)
 credentials = {}
 
-def creds():    
+def creds():
     if db_url:
         credentials['DATABASE_HOST'] = os.environ.get('DATABASE_HOST', None)
         credentials['DATABASE_USER'] = os.environ.get('DATABASE_USER', None)
@@ -16,6 +16,6 @@ def creds():
         credentials['DATABASE_USER'] = 'kuikpnusgzlgik'
         credentials['DATABASE_PASSWORD'] = '10ad97657b90e0238847c2d7586984cdf8e07e7105b71667027c3e08f66b2eb8'
         credentials['DATABASE_NAME'] = 'd9sc4gblm6vnsk'
-        
-    
+
+
     return credentials
