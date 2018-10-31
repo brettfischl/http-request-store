9662d870e9f5a25e1dbd1c1c3c5cdd2324168e59
brettfischl <brettfischl1@gmail.com>
2018-10-30 22:13:11 -0400

    allow cors

--- app.py
+++ app.py
@@ -7,8 +7,6 @@ import db
 @app.after_request
 def after_request(response):
   response.headers.add('Access-Control-Allow-Origin', '*')
-  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
   return response
 
 @app.route("/")
